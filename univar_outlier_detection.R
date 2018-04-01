67236f2a56edb8d5d9ce616b459fa8d18df9e8bd
Nouroz Rahman <nouroz_rahman@yahoo.com>
2018-04-01 16:43:09 +0600

    Update univar_outlier_detection.R

--- univar_outlier_detection.R
+++ univar_outlier_detection.R
@@ -1,5 +1,7 @@
-# OUTLIER DETECTION PACKAGE: https://rdrr.io/cran/univOutl/man/LocScaleB.html [LocScaleB]
-# FUNCTION FOR UNIVARIATE OUTLIER DETECTION USING "MAD" METHOD 
+# LIBRARY 
+library(robustbase) 
+
+# FUNCTION FOR UNIVARIATE OUTLIER DETECTION USING "MAD" METHOD  
 MAD_method = function(x){
   med = median(x)
   MAD = mad(x) 
@@ -18,26 +20,14 @@ IQR_method = function(x){
   return(c(lower, higher))
 }
 
+# FUNCTION FOR UNIVARIATE OUTLIER DETECTION USING "Adjusted Boxplot" METHOD
 AdjBox_method = function(x){
   adj_box = adjbox(x, main  = "Adjusted Boxplot")
   return(adj_box$fence)
 }
 
-# FUNCTION FOR UNIVARIATE OUTLIER DETECTION USING "Adjusted Boxplot" METHOD
-library(robustbase) 
-# a = c(-1,2,4,5,6,10,15,20,209,302)
-unique_driver_limit <- unique_driver_perc$perc_unique_drivers_served_by
-IQR_method(unique_driver_limit)
-MAD_method(unique_driver_limit)
-adj_box = adjbox(unique_driver_limit, main  = "Adjusted Boxplot")
-adj_box$fence
-plot(density(unique_driver_limit))
-x = unique_driver_limit[unique_driver_limit > 0.65 & unique_driver_limit < 0.80] 
-IQR_method(x)
-MAD_method(x) # SOWS BEST RESULTS 
-adj_box_new = adjbox(x, main  = "Adjusted Boxplot")
-adj_box_new$fence
-plot(density(x))
+# Test data: a = c(-1,2,4,5,6,10,15,20,209,302)
+
 
 
 
